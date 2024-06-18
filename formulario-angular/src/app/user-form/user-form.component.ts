965acf481b5bc6448a7c5d4abb2a01487482da69
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-18 10:36:23 -0300

    estrutura do form no templete e logica no ts

--- formulario-angular/src/app/user-form/user-form.component.ts
+++ formulario-angular/src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  userForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
   }
 
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      console.log('Form Submitted!', this.userForm.value);
+    }
+  }
 }
